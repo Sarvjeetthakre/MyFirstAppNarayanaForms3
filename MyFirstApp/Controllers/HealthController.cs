e1d9d3b950744bcbdfea9a76fe418f4d0ffc7392
hp <hp@LAPTOP-JVL05UDM>
2022-11-15 15:17:52 +0530

    Add project files.

---
+++ MyFirstApp/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstApp.Controllers
+{
+    public class HealthController : Controller
+    {
+        // GET: Health
+        public ActionResult Index()
+        {//in this health package form
+            return View();
+        }
+    }
+}
---
+++ MyFirstApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MyFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        { // in this enquiry form
+            return View();
+        }
+       
+
+        public ActionResult Products()
+        {
+            return View("OurCompanyProducts");
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
+
+
+
